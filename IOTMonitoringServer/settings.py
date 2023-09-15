99bc9ee3e93348d8690fa59e6372b2425904b766
Viviana Ojeda <m.ojedan@uniandes.edu.co>
2023-09-15 14:32:19 -0500

    actualiza ip3

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "52.91.190.7"]
+ALLOWED_HOSTS = ["localhost", "54.164.73.191"]
 
 
 # Application definition
