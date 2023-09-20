cb82dd21d58fd0c8ffcf01307b5bfd33e7f37837
Viviana Ojeda <m.ojedan@uniandes.edu.co>
2023-09-19 20:51:00 -0500

    update ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.164.73.191"]
+ALLOWED_HOSTS = ["localhost", "44.201.134.97"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "18.207.202.146",  # Dirección IP de la base de datos
+        "HOST": "3.86.218.123",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "44.204.172.168"
+MQTT_HOST = "18.208.151.182"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
